5f0e9d9830d0226a9ada0e907c8f2a598329ac9c
Librazy <librazy@foxmail.com>
2017-01-16 23:30:57 +0800

    UPDATE cleanup

--- src/main/java/think/rpgitems/PowerHandler.java
+++ src/main/java/think/rpgitems/PowerHandler.java
@@ -92,26 +92,6 @@ public class PowerHandler implements CommandHandler {
         sender.sendMessage(ChatColor.AQUA + Locale.get("message.power.ok"));
     }
 
-    @CommandString("rpgitem $n[] power tippedarrow $cooldown:i[] $effect:s[] $duration:i[] $amplifier:i[]")
-    @CommandDocumentation("$command.rpgitem.tippedarrow+PotionEffectType")
-    @CommandGroup("item_power_tippedarrow")
-    public void tippedarrow(CommandSender sender, RPGItem item, int cooldown, String effect, int duration, int amplifier) {
-        PowerTippedArrow pow = new PowerTippedArrow();
-        pow.item = item;
-        pow.cooldownTime = cooldown;
-        pow.duration = duration;
-        pow.amplifier = amplifier;
-        pow.type = PotionEffectType.getByName(effect);
-        if(pow.type == null){
-            sender.sendMessage(ChatColor.RED + String.format(Locale.get("message.error.effect"), effect));
-            return;
-        }
-        item.addPower(pow);
-        ItemManager.save(Plugin.plugin);
-        sender.sendMessage(ChatColor.AQUA + Locale.get("message.power.ok"));
-    }
-
-
     @CommandString("rpgitem $n[] power arrow $cooldown:i[]")
     @CommandDocumentation("$command.rpgitem.arrow.full")
     @CommandGroup("item_power_arrow")
@@ -582,6 +562,25 @@ public class PowerHandler implements CommandHandler {
         pow.item = item;
         pow.cooldownTime = cooldown;
         pow.distance = distance;
+        item.addPower(pow);
+        ItemManager.save(Plugin.plugin);
+        sender.sendMessage(ChatColor.AQUA + Locale.get("message.power.ok"));
+    }
+
+    @CommandString("rpgitem $n[] power tippedarrow $cooldown:i[] $effect:s[] $duration:i[] $amplifier:i[]")
+    @CommandDocumentation("$command.rpgitem.tippedarrow+PotionEffectType")
+    @CommandGroup("item_power_tippedarrow")
+    public void tippedarrow(CommandSender sender, RPGItem item, int cooldown, String effect, int duration, int amplifier) {
+        PowerTippedArrow pow = new PowerTippedArrow();
+        pow.item = item;
+        pow.cooldownTime = cooldown;
+        pow.duration = duration;
+        pow.amplifier = amplifier;
+        pow.type = PotionEffectType.getByName(effect);
+        if(pow.type == null){
+            sender.sendMessage(ChatColor.RED + String.format(Locale.get("message.error.effect"), effect));
+            return;
+        }
         item.addPower(pow);
         ItemManager.save(Plugin.plugin);
         sender.sendMessage(ChatColor.AQUA + Locale.get("message.power.ok"));
