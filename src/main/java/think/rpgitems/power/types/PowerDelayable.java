624725a75a0af52c3657bfcb95ba98f9e70803f5
ReinWD <ReinWDD@gmail.com>
2017-12-29 23:49:33 +0800

    Add new Interface "PowerDelayable"

--- src/main/java/think/rpgitems/power/types/PowerDelayable.java
+++ src/main/java/think/rpgitems/power/types/PowerDelayable.java
@@ -1,7 +1,12 @@
 package think.rpgitems.power.types;
 
 import org.bukkit.entity.Player;
+import think.rpgitems.commands.Property;
 
 public interface PowerDelayable extends Power{
-    void TriggerLater(Power power);
+    @Property(order = 4)
+    public int delay = 0;
+
+    default void TriggerLater(Power power,Player player){};
+
 }
