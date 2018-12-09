b60935f4992bda09c60c859af4d2e561621d44cb
Librazy <im@librazy.org>
2018-12-10 03:45:47 +0800

    [release] fix deflect

--- src/main/java/think/rpgitems/power/impl/PowerDeflect.java
+++ src/main/java/think/rpgitems/power/impl/PowerDeflect.java
@@ -2,7 +2,6 @@ package think.rpgitems.power.impl;
 
 import cat.nyaa.nyaacore.utils.TridentUtils;
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.Sound;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.*;
@@ -131,11 +130,12 @@ public class PowerDeflect extends BasePower implements PowerHitTaken, PowerRight
         event.setCancelled(true);
         p.setShooter(target);
         Events.registerProjectile(p.getEntityId(), getItem().getUID());
+        Vector velocity = p.getVelocity();
         Bukkit.getScheduler().runTaskLater(RPGItems.plugin, () -> {
             if (p instanceof Trident) {
                 TridentUtils.setTridentDealtDamage((Trident) p, false);
             }
-            p.setVelocity(target.getEyeLocation().getDirection().multiply(p.getVelocity().length()));
+            p.setVelocity(target.getEyeLocation().getDirection().multiply(velocity.length()));
             if (p instanceof Fireball) {
                 ((Fireball) p).setDirection(target.getEyeLocation().getDirection());
             }
