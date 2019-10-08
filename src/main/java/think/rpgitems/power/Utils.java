2fd8293b12ad20d31725503b895aee0bd5095570
ReinWD <ReinWDD@gmail.com>
2019-10-08 18:51:42 +0800

    fix compairing error

--- src/main/java/think/rpgitems/power/Utils.java
+++ src/main/java/think/rpgitems/power/Utils.java
@@ -155,7 +155,7 @@ public class Utils {
         return newEntities.stream().map(AngledEntity::getEntity).collect(Collectors.toList());
     }
 
-    private static class AngledEntity implements Comparable<Double>{
+    private static class AngledEntity implements Comparable<AngledEntity>{
         double angle;
         LivingEntity entity;
 
@@ -173,8 +173,8 @@ public class Utils {
         }
 
         @Override
-        public int compareTo(Double o) {
-            return ((Double)angle).compareTo(o);
+        public int compareTo(AngledEntity o) {
+            return Double.compare(angle, o.angle);
         }
     }
 
