6967c50a066a6f1d2280abd8187c79b08990c520
ReinWD <ReinWDD@gmail.com>
2020-01-15 22:39:35 +0800

    finish slot condition

--- src/main/java/think/rpgitems/power/cond/SlotCondition.java
+++ src/main/java/think/rpgitems/power/cond/SlotCondition.java
@@ -1,15 +1,21 @@
 package think.rpgitems.power.cond;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.scheduler.BukkitRunnable;
 import think.rpgitems.RPGItems;
 import think.rpgitems.data.LightContext;
+import think.rpgitems.item.ItemManager;
+import think.rpgitems.item.RPGItem;
 import think.rpgitems.power.*;
 
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 @Meta(marker = true)
@@ -104,57 +110,96 @@ public class SlotCondition extends BaseCondition<Void> {
                     return checkBoots(inventory, stack);
                 case ARMOR:
                     return checkHelmet(inventory, stack) || checkChestPlate(inventory, stack) ||
-                            checkLeggings(inventory, stack) || checkLeggings(inventory, stack);
+                            checkLeggings(inventory, stack) || checkBoots(inventory, stack);
                 case HAND:
                     return checkMainHand(inventory, stack) && checkOffHand(inventory, stack);
                 case BACKPACK:
                     return checkBackpack(inventory, stack);
                 case BELT:
-                    break;
+                    return checkBelts(inventory, stack);
                 case INVENTORY:
-                    break;
+                    return checkBelts(inventory, stack) || checkBackpack(inventory, stack);
                 case MAIN_HAND:
-                    break;
+                    return checkMainHand(inventory, stack);
                 case OFF_HAND:
-                    break;
+                    return checkOffHand(inventory, stack);
             }
             return false;
         }
 
-        private boolean checkBackpack(PlayerInventory inventory, ItemStack stack) {
+        private boolean checkBelts(PlayerInventory inventory, ItemStack stack) {
+            UUID uniqueId = inventory.getHolder().getUniqueId();
             ItemStack[] contents = inventory.getContents();
-            ItemStack[] itemStacks = Arrays.copyOfRange(contents, 0, 27);
-            return false;
+            return cachedContainsOr(backpackCache, uniqueId, stack, () -> Arrays.copyOfRange(contents, 0, 9));
+        }
+
+        Cache<UUID, ItemStack[]> backpackCache = CacheBuilder.newBuilder()
+                .expireAfterWrite(50, TimeUnit.MICROSECONDS)
+                .build();
+        Cache<UUID, ItemStack[]> beltCache = CacheBuilder.newBuilder()
+                .expireAfterWrite(50, TimeUnit.MICROSECONDS)
+                .build();
+
+        private boolean checkBackpack(PlayerInventory inventory, ItemStack stack) {
+            UUID uniqueId = inventory.getHolder().getUniqueId();
+            ItemStack[] contents = inventory.getContents();
+            return cachedContainsOr(backpackCache, uniqueId, stack, () -> Arrays.copyOfRange(contents, 10, 36));
+         }
+
+        private boolean cachedContainsOr(Cache<UUID, ItemStack[]> backpackCache, UUID uuid, ItemStack stack, Supplier<ItemStack[]> supplier) {
+            ItemStack[] ifPresent = backpackCache.getIfPresent(uuid);
+            if (ifPresent == null){
+                ifPresent = supplier.get();
+                backpackCache.put(uuid, ifPresent);
+            }
+            return Stream.of(ifPresent).anyMatch(itemStack -> match(itemStack, stack));
         }
 
         private boolean checkMainHand(PlayerInventory inventory, ItemStack stack) {
             ItemStack itemInMainHand = inventory.getItemInMainHand();
-            return stack.equals(itemInMainHand);
+            return match(itemInMainHand, stack);
         }
 
         private boolean checkOffHand(PlayerInventory inventory, ItemStack stack) {
             ItemStack itemInOffHand = inventory.getItemInOffHand();
-            return stack.equals(itemInOffHand);
+            return match(itemInOffHand, stack);
         }
 
         private boolean checkBoots(PlayerInventory inventory, ItemStack stack) {
             ItemStack boots = inventory.getBoots();
-            return stack.equals(boots);
+            return match(boots, stack);
         }
 
         private boolean checkLeggings(PlayerInventory inventory, ItemStack stack) {
             ItemStack leggings = inventory.getLeggings();
-            return stack.equals(leggings);
+            return match(leggings, stack);
         }
 
         private boolean checkChestPlate(PlayerInventory inventory, ItemStack stack) {
             ItemStack chestPlate = inventory.getChestplate();
-            return stack.equals(chestPlate);
+            return match(chestPlate, stack);
         }
 
         private boolean checkHelmet(PlayerInventory inventory, ItemStack stack) {
             ItemStack helmet = inventory.getHelmet();
-            return stack.equals(helmet);
+            return match(helmet, stack);
+        }
+
+        public boolean match(ItemStack itemStack, ItemStack stack) {
+            if (stack == null) return itemStack == null;
+            if (itemStack != null && !stack.isSimilar(itemStack)) {
+                return false;
+            }
+            Optional<RPGItem> itemUsed = ItemManager.toRPGItem(stack);
+            Optional<RPGItem> stackItem = ItemManager.toRPGItem(itemStack);
+            if (itemUsed.isPresent()) {
+                if (!stackItem.isPresent()) {
+                    return false;
+                }
+                if (!itemUsed.get().equals(stackItem.get()))
+                    return false;
+            }
+            return true;
         }
     }
 }
