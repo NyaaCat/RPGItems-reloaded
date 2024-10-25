e2a23601b852fc9b7968869cd9e1139641aaadb8
Clexus <528373858@qq.com>
2024-10-25 19:47:48 +0800

    Add potion level and duration for potionhit message

--- src/main/java/think/rpgitems/power/impl/PotionHit.java
+++ src/main/java/think/rpgitems/power/impl/PotionHit.java
@@ -74,7 +74,7 @@ public class PotionHit extends BasePower {
 
     @Override
     public String displayText() {
-        return I18n.formatDefault("power.potionhit", (int) ((1d / (double) getChance()) * 100d), getType().getName().toLowerCase().replace('_', ' '));
+        return I18n.formatDefault("power.potionhit", (int) ((1d / (double) getChance()) * 100d), getType().key().value(), (getAmplifier()+1), (float)(getDuration()/20));
     }
 
     /**
